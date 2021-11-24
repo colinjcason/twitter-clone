e263f05e1ce2caa9554c9a5f964065e6eb63dfc0
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2021-11-24 15:32:09 -0800

    added netlify auth widget

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,50 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { auth, init, logIn as authLogin, logOut as authLogOut } from '../lib/auth'
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    init(user => {
+      setUser(user)
+    })
+
+    auth.on('login', (user) => {
+      setUser(user)
+    })
+
+    return () => {
+      auth.off('login', setUser)
+    }
+  }, [])
+
+  function logIn() {
+    authLogin(user => {
+      setUser(user)
+    })
+  }
+
+  function logOut() {
+    authLogOut(() => {
+      setUser(undefined)
+    })
+  }
+  
+  const contextValue = {
+    user,
+    logIn,
+    logOut
+  }
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
